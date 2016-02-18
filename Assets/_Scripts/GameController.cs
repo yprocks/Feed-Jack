4bf05d47e2561cfb2554a99a9dfb671a036c3045
yprocks <yppatel181093@gmail.com>
2016-02-18 16:02:51 -0500

    added UI

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class GameController : MonoBehaviour {
@@ -6,7 +8,15 @@ public class GameController : MonoBehaviour {
 	//public variables
 	public GameObject gem;
 
+	public Text scoreText;
+	public Text gemText;
+	public Text coinText;
+
+	public Slider healthHUD;
+
+
 	//private variables
+
 
 
 	// Use this for initialization
