21b38edcb63bbc7f198df1ce5253f8882f27fa4c
yprocks <yppatel181093@gmail.com>
2016-02-17 22:57:24 -0500

    added Bananas

--- Assets/_Scripts/BananaSpawn.cs
+++ Assets/_Scripts/BananaSpawn.cs
@@ -1,10 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinSpawner : MonoBehaviour {
+public class BananaSpawn : MonoBehaviour {
 
 	//public variables
-	public GameObject coin;
+	public GameObject banana;
 
 	//private variables
 	private Transform _transform;
@@ -19,14 +19,15 @@ public class CoinSpawner : MonoBehaviour {
 
 	// Update is called once per frame
 	void SpawnCoins () {
-		
-		for(int i = 0; i < 2; i++){
-			for(int j = 0; j < 5; j++){
-				Instantiate (coin, currentPosition, Quaternion.identity);
-				currentPosition.x += 25;
-			}
-			Reset ();
-			currentPosition.y += 26;
+
+		float yPos = Random.Range(currentPosition.y-5, currentPosition.y+15);
+		float xPos = Random.Range (currentPosition.x - 15, currentPosition.x + 5);
+
+		for(int i = 0; i < Random.Range(1,6); i++){
+			Instantiate (
+				banana, new Vector2(xPos, yPos), Quaternion.identity
+			);
+		xPos += 15;
 		}
 
 	}
